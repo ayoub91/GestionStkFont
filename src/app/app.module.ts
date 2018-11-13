7b4ee9b127de1ee2227fb65150d90538b8fa5f44
ayoub <ayoub.boudad91@gmail.com>
2018-11-13 20:27:56 +0000

    add compenant Produit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ProduitMockService } from './produit/produit.mock.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProduitMockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/produit/produit.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ProduitMockService } from './produit.mock.service';
+import { Produit } from '../shared/produit';
+
+@Component({
+  selector: 'app-produit',
+  templateUrl: './produit.component.html',
+  styleUrls: ['./produit.component.css']
+})
+export class ProduitComponent implements OnInit {
+
+  produits : Produit[];
+
+  constructor(private produitMockService : ProduitMockService) { 
+
+  }
+
+  ngOnInit() {
+    this.produits = this.produitMockService.getProduit();
+  }
+  
+
+}
